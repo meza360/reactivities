4d8a0d075b0de007596e6ae56f079882d0d0cd80
Giovani Meza <giovanidav5@outlook.com>
2022-07-05 21:15:33 -0600

    Refactoring
    Adds dedicated folder for domain entities

---
+++ backend/Domain/Entities/Activity.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Domain
+{
+    public class Activity
+    {
+        [Key]
+        public Int16 Id { get; set; }
+        public string Title { get; set; }
+        
+        [Column(TypeName ="Date")]
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
