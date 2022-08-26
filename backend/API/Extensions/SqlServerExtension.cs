906b0e702188232c873ea740fd7a595578909928
Giovani Meza <giovanidav5@outlook.com>
2022-08-26 13:02:48 -0600

    Feature
    Enables backend services to connect to localdb from mssqlserver

---
+++ backend/API/Extensions/SqlServerExtension.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class SqlServerExtension
+    {
+        public static IServiceCollection AddSqlServerServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(
+                options => {
+                    options.UseSqlServer(config.GetConnectionString("LocalDb"));
+                }
+            );
+            return services;
+        }
+    }
+}
